2b07a5839eae47cc141f90b9fa10fb2750b1298d
maumora02 <20194348@aloe.ulima.edu.pe>
2024-11-06 13:47:20 -0500

    Update App.js 06/11

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import NavBar2 from './components/NavBar2';
+import NavBar3 from './components/NavBar3';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import CategoriesSection from './components/CategoriesSection';
 
 const App = () => {
     return (
-    
         <Router>
-            <NavBar2 /> 
+            <NavBar2 />
             <NavBar />
-            <Routes> 
-                <Route path="/" element={<h1>¡Bienvenido a mi aplicación!</h1>} />
+            <NavBar3 />
+            <CategoriesSection />
+            <Routes>
+                <Route path="/" element={<h1>¡Bienvenido a mi aplicación!</h1>} />               
                 <Route path="/about" element={<h1>Esta es la página Acerca de.</h1>} />
                 <Route path="/contact" element={<h1>Esta es la página de Contacto.</h1>} />
                 <Route path="/terms" element={<h1>Políticas y Condiciones</h1>} />
